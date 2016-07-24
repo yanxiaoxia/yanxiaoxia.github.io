9fa99e65f1a8fa723569be3e218c50fc96d7a266
yanxiaoxia <1363809077@qq.com>
2016-07-24 12:20:23 +0800

    js

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(function($){
+    $('#fullpage').fullpage({
+
+    });
+    var mySwiper = new Swiper('.swiper-container',{
+        slidesPerView : 3,
+        slidesPerGroup : 3,
+        prevButton:'.swiper-button-prev',
+        nextButton:'.swiper-button-next',
+    })
+})
+
+
